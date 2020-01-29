24e5a262ad752518b8d6ea549b6e0a875dfafcf9
Devin W. Leaman <dleaman212@gmail.com>
2020-01-28 19:07:30 -0600

    Updated polls component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -50,7 +51,8 @@ import { PollsComponent } from './polls/polls.component';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [CookieService],
   bootstrap: [AppComponent]
--- src/app/polls/polls.component.ts
+++ src/app/polls/polls.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { Poll } from '../models/polls';
+import { PollService } from '../poll.service';
 
 @Component({
   selector: 'app-polls',
@@ -7,11 +9,18 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./polls.component.scss']
 })
 export class PollsComponent implements OnInit {
+  polls: Poll[] = [];
 
-  constructor(private cookies: CookieService) { }
+  constructor(private cookies: CookieService, private pollService: PollService) { }
 
   ngOnInit() {
-    console.log(this.cookies.get('client-id'));
+    // this.
+    console.log(`this.cookies.get('client-id') = ${this.cookies.get('client-id')}`);
+    this.getPolls();
   }
 
+  getPolls(): void {
+    this.pollService.getPolls()
+      .subscribe(polls => this.polls = polls);
+  }
 }
